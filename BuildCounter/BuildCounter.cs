bfed2ad71294e503523478c5aad36aaea952de3c
brokenmass <massa84@gmail.com>
2021-03-03 15:55:00 +0000

    BuildCounter initial release

---
+++ BuildCounter/BuildCounter.cs
@@ -0,0 +1,67 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BuildCounter
+{
+
+    [BepInPlugin("com.brokenmass.plugin.DSP.BuildCounter", "BuildCounter", "1.0.0")]
+    public class BuildCounter : BaseUnityPlugin
+    {
+        Harmony harmony;
+
+        internal static readonly string SPACING = new String(' ', 5);
+
+        internal void Awake()
+        {
+            harmony = new Harmony("com.brokenmass.plugin.DSP.BuildCounter");
+            try
+            {
+                harmony.PatchAll(typeof(BuildCounter));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        internal void OnDestroy()
+        {
+            // For ScriptEngine hot-reloading
+            harmony.UnpatchSelf();
+        }
+
+        [HarmonyPostfix, HarmonyPriority(Priority.Last), HarmonyPatch(typeof(PlayerAction_Build), "UpdatePreviews")]
+        public static void UpdatePreviews_Postfix(ref PlayerAction_Build __instance)
+        {
+            if (__instance.buildPreviews.Count > 0)
+            {
+                var counter = new Dictionary<string, int>();
+
+                foreach (var buildPreview in __instance.buildPreviews)
+                {
+                    var name = buildPreview.item.name;
+
+                    if(!counter.ContainsKey(name))
+                    {
+                        counter.Add(name, 0);
+                    }
+
+                    counter[name]++;
+                }
+
+                __instance.cursorText += $"\nUsing:";
+                foreach (var entry in counter)
+                {
+                    __instance.cursorText += $"\n{SPACING}- {entry.Value} x {entry.Key}";
+                }
+            }
+
+        }
+    }
+}
---
+++ BuildCounter/README.md
@@ -0,0 +1,17 @@
+# BuildCounter
+
+With this mod installed your cursor will show the amount of items being used by the current build order.  
+
+Fully compatible with [CopyInserter](https://dsp.thunderstore.io/package/thisisbrad/CopyInserters/) and [AdvancedBuildDestruct](https://dsp.thunderstore.io/package/xiaoye97/AdvancedBuildDestruct/).
+
+![BuildCounter-1](https://github.com/DysonSphereMod/QOL/blob/master/BuildCounter/screenshot.jpg?raw=true)
+![BuildCounter-2](https://github.com/DysonSphereMod/QOL/blob/master/BuildCounter/screenshot2.jpg?raw=true)
+
+## Changelog
+
+### v1.0.0
+- Initial Release
+
+## Special thanks
+
+- <a href="https://icons8.com/icon/gCiuDtPGHwGJ/counter">Counter icon by Icons8</a>
