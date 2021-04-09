fd8550d56e0153b55837a6c098e54afefc298444
brokenmass <massa84@gmail.com>
2021-04-09 04:35:30 +0100

    fix issue #68

--- MultiBuild/Properties/AssemblyInfo.cs
+++ MultiBuild/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.2")]
-[assembly: AssemblyFileVersion("2.3.2")]
+[assembly: AssemblyVersion("2.3.3")]
+[assembly: AssemblyFileVersion("2.3.3")]
